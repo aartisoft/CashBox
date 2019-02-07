caa6fb76850e0fb6734856461bb4aa73481f697f
Dominik <diedrich.dominik@yahoo.com>
2019-02-07 17:19:21 +0100

    Menu Settings AddPrinter Search

--- app/src/main/java/com/example/dd/cashbox/MS_AddPrinter_Search.java
+++ app/src/main/java/com/example/dd/cashbox/MS_AddPrinter_Search.java
@@ -2,7 +2,6 @@ package com.example.dd.cashbox;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -12,9 +11,8 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class MS_AddPrinter extends AppCompatActivity {
+public class MS_AddPrinter_Search extends AppCompatActivity {
 
-    private FloatingActionButton m_fab;
     private ListView m_listView;
     private View m_decorView;
     private int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -27,27 +25,19 @@ public class MS_AddPrinter extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ms_addprinter);
+        setContentView(R.layout.activity_ms_addprinter_search);
 
         //init variables
-        m_fab = (FloatingActionButton) findViewById(R.id.ms_addprinter_add);
         m_listView = (ListView) findViewById(R.id.ms_addprinter_listview);
         m_decorView = getWindow().getDecorView();
         m_decorView.setSystemUiVisibility(uiOptions);
 
         //set header
-        Toolbar toolbar = findViewById(R.id.toolbar_ms_addprinter);
+        Toolbar toolbar = findViewById(R.id.toolbar_ms_addprinter_search);
+        toolbar.setNavigationIcon(R.drawable.ic_close_white_24dp);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        m_fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MS_AddPrinter.this, MS_AddPrinter_Search.class);
-                startActivity(intent);
-            }
-        });
 
         // Set an item click listener for ListView
         /*m_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -75,7 +65,7 @@ public class MS_AddPrinter extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                Intent intent = new Intent(MS_AddPrinter.this, MenuSettings.class);
+                Intent intent = new Intent(MS_AddPrinter_Search.this, MS_AddPrinter.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
                 finish();
