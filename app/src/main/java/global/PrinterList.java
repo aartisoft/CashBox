dcad251ace9a44b6044735e9ee25e2e7ffb9faa3
Dominik <diedrich.dominik@yahoo.com>
2019-02-08 22:06:35 +0100

    kommentare entfernt

--- app/src/main/java/global/PrinterList.java
+++ app/src/main/java/global/PrinterList.java
@@ -9,23 +9,6 @@ import objects.ObjPrinter;
 
 public abstract class PrinterList extends Application {
 
-
-
     public static List<ObjPrinter> m_lstPrinter = new ArrayList<ObjPrinter>();
 
-    /*public void addEntryPrinterList(String p_strName, String p_strTarget, String p_strCategory){
-        Printer printer = new Printer();
-        printer.m_strName = p_strName;
-        printer.m_strTarget = p_strTarget;
-        printer.m_strCategory = p_strCategory;
-
-        m_lstPrinter.add(printer);
-    }
-    public void delEntryPrinterList(){
-        //m_lstPrinter.
-    }
-
-    public List<Printer> getPrinterList(){
-        return this.m_lstPrinter;
-    }*/
 }
