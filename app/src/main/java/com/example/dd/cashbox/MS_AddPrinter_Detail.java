90075f6df66877d2315871322a43ac635f9fc779
Dominik <diedrich.dominik@yahoo.com>
2019-02-09 11:49:01 +0100

    Druckerinformationen

---
+++ app/src/main/java/com/example/dd/cashbox/MS_AddPrinter_Detail.java
@@ -0,0 +1,80 @@
+package com.example.dd.cashbox;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import adapter.ListViewPrinterAdapter;
+import global.PrinterList;
+
+public class MS_AddPrinter_Detail extends AppCompatActivity {
+
+    private ListViewPrinterAdapter m_adapter;
+    private View m_decorView;
+    private int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ms_addprinter_detail);
+
+        //init variables
+        m_decorView = getWindow().getDecorView();
+        m_decorView.setSystemUiVisibility(uiOptions);
+
+        //set header
+        Toolbar toolbar = findViewById(R.id.toolbar_ms_addprinter_detail);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //set TextView
+
+
+        // Set an item click listener for ListView
+        /*m_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the selected item text from ListView
+                String selectedItem = (String) parent.getItemAtPosition(position);
+
+                if (selectedItem.equals(R.string.src_DruckerHinzufuegen)) {
+                    //startActivity(new Intent(this, MenuSettings.class));
+                }
+            }
+        });*/
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            m_decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(MS_AddPrinter_Detail.this, MS_AddPrinter.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
