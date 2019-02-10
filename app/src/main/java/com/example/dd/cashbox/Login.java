fe057e3dc82bea2dc4ed9b5bbcec62dcfd20ba8c
Dominik <diedrich.dominik@yahoo.com>
2019-02-10 19:26:23 +0100

    pretty code

--- app/src/main/java/com/example/dd/cashbox/Login.java
+++ app/src/main/java/com/example/dd/cashbox/Login.java
@@ -19,6 +19,12 @@ public class Login extends AppCompatActivity implements OnClickListener {
     private View m_decorView;
     private EditText m_tbLicenceKey;
     private Button m_btnLogin;
+    private int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,62 +37,14 @@ public class Login extends AppCompatActivity implements OnClickListener {
         m_btnLogin = findViewById(R.id.btnLogin);
         m_btnLogin.setEnabled(false);
 
-        //hide header and footer
+        //set UI
         m_decorView = getWindow().getDecorView();
-        final int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        //m_decorView.setSystemUiVisibility(uiOptions);
+        m_decorView.setSystemUiVisibility(uiOptions);
 
-        //SoftKeyBoardListener
-        m_decorView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                Rect r = new Rect();
-                m_decorView.getWindowVisibleDisplayFrame(r);
-                int screenHeight = m_decorView.getRootView().getHeight();
-
-                // r.bottom is the position above soft keypad or device button.
-                // if keypad is shown, the r.bottom is smaller than that before.
-                int keypadHeight = screenHeight - r.bottom;
-
-                //Log.d(TAG, "keypadHeight = " + keypadHeight);
-
-                if (keypadHeight > screenHeight * 0.10) {
-                    // keyboard is opened
-                }
-                else {
-                    //keyboard is closed
-                    m_tbLicenceKey.setCursorVisible(false);
-                    m_decorView.setSystemUiVisibility(uiOptions);
-                }
-            }
-        });
-
-        //init events
+        //set Listener
+        m_decorView.getViewTreeObserver().addOnGlobalLayoutListener(softkeyboardOnGlobalLayoutListener);
         m_btnLogin.setOnClickListener(this);
-        m_tbLicenceKey.addTextChangedListener(new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {
-            }
-
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-                if(!m_tbLicenceKey.getText().equals("")){
-                    m_btnLogin.setEnabled(true);
-                }
-                else{
-                    m_btnLogin.setEnabled(false);
-                }
-            }
-        });
+        m_tbLicenceKey.addTextChangedListener(keyTextWatcher);
     }
 
     @Override
@@ -109,4 +67,47 @@ public class Login extends AppCompatActivity implements OnClickListener {
                 | View.SYSTEM_UI_FLAG_LOW_PROFILE;
         m_decorView.setSystemUiVisibility(uiOptions);
     }
+
+    private ViewTreeObserver.OnGlobalLayoutListener softkeyboardOnGlobalLayoutListener = new ViewTreeObserver.OnGlobalLayoutListener(){
+        @Override
+        public void onGlobalLayout() {
+            Rect r = new Rect();
+            m_decorView.getWindowVisibleDisplayFrame(r);
+            int screenHeight = m_decorView.getRootView().getHeight();
+
+            // r.bottom is the position above soft keypad or device button.
+            // if keypad is shown, the r.bottom is smaller than that before.
+            int keypadHeight = screenHeight - r.bottom;
+
+            //Log.d(TAG, "keypadHeight = " + keypadHeight);
+
+            if (keypadHeight > screenHeight * 0.10) {
+                // keyboard is opened
+            }
+            else {
+                //keyboard is closed
+                m_tbLicenceKey.setCursorVisible(false);
+                m_decorView.setSystemUiVisibility(uiOptions);
+            }
+        }
+    };
+
+    private TextWatcher keyTextWatcher = new TextWatcher(){
+        public void afterTextChanged(Editable s) {
+        }
+
+        public void beforeTextChanged(CharSequence s, int start,
+                                      int count, int after) {
+        }
+
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+            if(!m_tbLicenceKey.getText().equals("")){
+                m_btnLogin.setEnabled(true);
+            }
+            else{
+                m_btnLogin.setEnabled(false);
+            }
+        }
+    };
 }
