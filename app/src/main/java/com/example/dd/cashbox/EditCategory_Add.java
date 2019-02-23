640c126cf509a32549e5c363ac92e6a0d1a53fda
Dominik <diedrich.dominik@yahoo.com>
2019-02-23 22:38:40 +0100

    Edit Category Add implementation

---
+++ app/src/main/java/com/example/dd/cashbox/EditCategory_Add.java
@@ -0,0 +1,135 @@
+package com.example.dd.cashbox;
+
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+
+import global.GlobVar;
+
+public class EditCategory_Add extends AppCompatActivity {
+
+    private FloatingActionButton m_fab;
+    private EditText m_EditTextName;
+    private View m_decorView;
+    private int m_uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        hideSystemUI(getWindow());
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editcategory_add);
+
+        //init variables
+        m_fab = findViewById(R.id.ms_addprinter_searchok);
+        m_EditTextName = findViewById(R.id.editcategory_add_tvname);
+        m_decorView = getWindow().getDecorView();
+
+        //set UI
+        m_decorView.setSystemUiVisibility(m_uiOptions);
+        Toolbar toolbar = findViewById(R.id.toolbar_editcategory_add);
+        toolbar.setNavigationIcon(R.drawable.ic_close_white_24dp);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        m_fab.setEnabled(false);
+
+        //set Listener
+        m_decorView.getViewTreeObserver().addOnGlobalLayoutListener(softkeyboardOnGlobalLayoutListener);
+        m_EditTextName.setOnEditorActionListener(DoneOnEditorActionListener);
+    }
+
+    private View.OnClickListener fabMinusOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    private OnEditorActionListener DoneOnEditorActionListener = new OnEditorActionListener(){
+
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                GlobVar.m_iTables = Integer.parseInt(m_EditTextName.getText().toString());
+            }
+            return false;
+        }
+    };
+
+    private ViewTreeObserver.OnGlobalLayoutListener softkeyboardOnGlobalLayoutListener = new ViewTreeObserver.OnGlobalLayoutListener(){
+        @Override
+        public void onGlobalLayout() {
+            Rect r = new Rect();
+            m_decorView.getWindowVisibleDisplayFrame(r);
+            int screenHeight = m_decorView.getRootView().getHeight();
+
+            // r.bottom is the position above soft keypad or device button.
+            // if keypad is shown, the r.bottom is smaller than that before.
+            int keypadHeight = screenHeight - r.bottom;
+
+            //Log.d(TAG, "keypadHeight = " + keypadHeight);
+
+            if (keypadHeight > screenHeight * 0.5) {
+                // keyboard is opened
+            }
+            else {
+                //keyboard is closed
+                m_decorView.setSystemUiVisibility(m_uiOptions);
+            }
+        }
+    };
+
+    public void hideSystemUI(Window window) {
+        m_decorView = window.getDecorView();
+        final int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LOW_PROFILE;
+        m_decorView.setSystemUiVisibility(uiOptions);
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            m_decorView.setSystemUiVisibility(m_uiOptions);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(EditCategory_Add.this, EditCategory.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
