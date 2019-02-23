872ddd479deea873ae90fd5955468bf5b0fa3fef
Dominik <diedrich.dominik@yahoo.com>
2019-02-23 20:33:44 +0100

    Edit Category implementation

--- app/src/main/java/com/example/dd/cashbox/EditCategory.java
+++ app/src/main/java/com/example/dd/cashbox/EditCategory.java
@@ -1,12 +1,18 @@
 package com.example.dd.cashbox;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,15 +20,24 @@ import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.TextView.OnEditorActionListener;
 
+import java.util.ArrayList;
+
+import adapter.RecyclerViewCategoryAdapter;
 import global.GlobVar;
+import objects.ObjCategory;
+import recyclerview.RecyclerItemTouchHelper;
 
-public class EditCategory extends AppCompatActivity {
+public class EditCategory extends AppCompatActivity implements RecyclerItemTouchHelper.RecyclerItemTouchHelperListener {
 
+    private ArrayList<ObjCategory> m_lstCategoryList;
+    private RecyclerViewCategoryAdapter m_adapter;
     private RecyclerView m_recyclerview;
+    private LinearLayout m_linearlayour;
     private FloatingActionButton m_fab_plus;
     private View m_decorView;
     private int m_uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -40,6 +55,7 @@ public class EditCategory extends AppCompatActivity {
         setContentView(R.layout.activity_editcategory);
 
         //init variables
+        m_linearlayour = findViewById(R.id.editcategory_linearlayout);
         m_recyclerview = findViewById(R.id.editcategory_recycler_view);
         m_fab_plus = findViewById(R.id.editcategory_fab);
         m_decorView = getWindow().getDecorView();
@@ -50,6 +66,20 @@ public class EditCategory extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //set RecyclerView
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        m_recyclerview.setLayoutManager(mLayoutManager);
+        m_recyclerview.setItemAnimator(new DefaultItemAnimator());
+        m_recyclerview.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        m_recyclerview.setAdapter(m_adapter);
+
+        //set item touch helper
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new RecyclerItemTouchHelper(0, ItemTouchHelper.LEFT, this);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(m_recyclerview);
+
+        //prepare Category
+
 
         //set Listener
         m_fab_plus.setOnClickListener(fabPlusOnClickListener);
@@ -97,5 +127,39 @@ public class EditCategory extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+        if (viewHolder instanceof RecyclerViewCategoryAdapter.MyViewHolder) {
+            // get the removed item name to display it in snack bar
+            String name = GlobVar.m_lstCategory.get(viewHolder.getAdapterPosition()).getName();
+
+            // backup of removed item for undo purpose
+            final ObjCategory deletedItem = GlobVar.m_lstCategory.get(viewHolder.getAdapterPosition());
+            final int deletedIndex = viewHolder.getAdapterPosition();
+
+            // remove the item from recycler view
+            m_adapter.removeItem(viewHolder.getAdapterPosition());
+
+            // showing snack bar with Undo option
+            Snackbar snackbar = Snackbar
+                    .make(m_linearlayour, name + " removed from cart!", Snackbar.LENGTH_LONG);
+            snackbar.setAction("UNDO", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    // undo is selected, restore the deleted item
+                    m_adapter.restoreItem(deletedItem, deletedIndex);
+                }
+            });
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.show();
+        }
+    }
+
+    private void prepareCategory(){
+
+        //m_lstCategoryList = GlobVar.m_lstCategroy;
     }
 }
--- app/src/main/java/global/GlobVar.java
+++ app/src/main/java/global/GlobVar.java
@@ -13,6 +13,6 @@ public abstract class GlobVar extends Application {
     public static List<ObjPrinter> m_lstPrinter = new ArrayList<ObjPrinter>();
     public static String m_strBedienername = "Bedienername";
     public static int m_iTables = 0;
-    public static List<ObjCategory> m_lstCategroy = new ArrayList<ObjCategory>();
+    public static List<ObjCategory> m_lstCategory = new ArrayList<ObjCategory>();
 
 }
