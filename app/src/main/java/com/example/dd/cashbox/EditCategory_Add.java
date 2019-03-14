29f292b619147b7c55c2a1b0500b0c77a3308c86
Dominik <diedrich.dominik@yahoo.com>
2019-03-14 21:53:01 +0100

    main page

--- app/src/main/java/com/example/dd/cashbox/EditCategory_Add.java
+++ app/src/main/java/com/example/dd/cashbox/EditCategory_Add.java
@@ -251,7 +251,7 @@ public class EditCategory_Add extends AppCompatActivity implements ChooseColorDi
 
     private void showColorDialog() {
         FragmentManager fm = getSupportFragmentManager();
-        ChooseColorDialogFragment chooseColorDialogFragment = ChooseColorDialogFragment.newInstance("Some Title", 0);
+        ChooseColorDialogFragment chooseColorDialogFragment = ChooseColorDialogFragment.newInstance("Some Title", 11022900);
         chooseColorDialogFragment.show(fm, "fragment_edit_name");
 
     }
