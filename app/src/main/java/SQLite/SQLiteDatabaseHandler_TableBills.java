5dbb4a755f0d7314aea4c62e0bb52c65a03addd8
Dominik <diedrich.dominik@yahoo.com>
2019-03-24 10:16:10 +0100

    sql tablebills

--- app/src/main/java/SQLite/SQLiteDatabaseHandler_TableBills.java
+++ app/src/main/java/SQLite/SQLiteDatabaseHandler_TableBills.java
@@ -47,8 +47,8 @@ public class SQLiteDatabaseHandler_TableBills extends SQLiteOpenHelper {
                 + "id INTEGER PRIMARY KEY AUTOINCREMENT, " + "tablename TEXT, "
                 + "billnr INTEGER, " + "cashiername TEXT, "
                 + "billingdate TEXT, " + "category TEXT, " + "product TEXT, "
-                + "quantity INTEGER, " + "printermac TEXT, " + "printed INT, "
-                + "canceled INT, " + "paid INTEGER)";
+                + "quantity INTEGER, " + "printermac TEXT, " + "printed INTEGER, "
+                + "canceled INTEGER, " + "paid INTEGER)";
 
         db.execSQL(CREATION_TABLE);
     }
@@ -123,114 +123,62 @@ public class SQLiteDatabaseHandler_TableBills extends SQLiteOpenHelper {
         }
     }
 
- /*   public ObjProduct getProduct(int id) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_NAME, // a. table
-                COLUMNS, // b. column names
-                " id = ?", // c. selections
-                new String[] { String.valueOf(id) }, // d. selections args
-                null, // e. group by
-                null, // f. having
-                null, // g. order by
-                null); // h. limit
-
-        if (cursor != null)
-            cursor.moveToFirst();
-
-        ObjProduct product = new ObjProduct();
-        product.setName(cursor.getString(1));
-        product.setVK(Float.parseFloat(cursor.getString(2)));
-        product.setbPAWN(Boolean.parseBoolean(cursor.getString(3)));
-        product.setPAWN(Float.parseFloat(cursor.getString(4)));
-        product.setEnabled(Boolean.parseBoolean(cursor.getString(5)));
-
-        return product;
-    }
-
-    public void addProduct(ObjProduct product) {
+    public void addTableBill(int p_iTable, int p_iBill) {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(KEY_NAME, product.getName());
-        values.put(KEY_VK, product.getVK());
 
-        int bpawn = product.getbPawn() ? 1 : 0;
-        values.put(KEY_BPAWN, bpawn);
+        //get bill
+        int iBill = 0;
+        for(ObjBill objBill : GlobVar.g_lstTableBills.get(p_iTable)){
+            if(objBill.getBillNr() == p_iBill){
+                break;
+            }
+            iBill++;
+        }
 
-        values.put(KEY_PAWN, product.getPawn());
+        for(ObjBillProduct objproduct : GlobVar.g_lstTableBills.get(p_iTable).get(iBill).m_lstProducts){
+            ContentValues values = new ContentValues();
 
-        int key_enabled = product.getEnabled() ? 1 : 0;
-        values.put(KEY_ENABLED, key_enabled);
+            //product not saved yet
+            if(!objproduct.getSqlSaved()){
+                values.put(KEY_TABLENAME, p_iTable);
+                values.put(KEY_BILLNR, p_iBill);
+                values.put(KEY_CASHIERNAME, GlobVar.g_lstTableBills.get(p_iTable).get(p_iBill).getCashierName());
+                //values.put(KEY_BILLINGDATE, GlobVar.g_lstTableBills.get(p_iTable).get(p_iBill).getBillingDate());
+                values.put(KEY_CATEGORY, objproduct.getCategory());
+                values.put(KEY_PRODUCT, objproduct.getProduct().getName());
+                values.put(KEY_CATEGORY, objproduct.getCategory());
+                values.put(KEY_QUANTITY, objproduct.getQuantity());
+                values.put(KEY_PRINTERMAC, objproduct.getPrinter().getMacAddress());
+                values.put(KEY_PRINTED, objproduct.getPrinted());
+                values.put(KEY_CANCELED, objproduct.getCanceled());
+                values.put(KEY_PAID, objproduct.getPaid());
+            }
 
-        values.put(KEY_CATEGORY, product.getCategory());
+            //only if quantity has changed
+            if(objproduct.getqlChanged()){
 
-        // insert
-        db.insert(TABLE_NAME,null, values);
-        db.close();
+                values.put(KEY_QUANTITY, objproduct.getQuantity());
+
+                int i = db.update(TABLE_NAME, // table
+                        values, // column/value
+                        "billnr = ?", // selections
+                        new String[] { String.valueOf(p_iBill) });
+            }
+
+            // insert
+            db.insert(TABLE_NAME,null, values);
+            db.close();
+        }
     }
-
-    public int updateProduct(String oldname, ObjProduct product) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(KEY_NAME, product.getName());
-        values.put(KEY_VK, product.getVK());
-
-        int bpawn = product.getbPawn() ? 1 : 0;
-        values.put(KEY_BPAWN, bpawn);
-
-        values.put(KEY_PAWN, product.getPawn());
-
-        int key_enabled = product.getEnabled() ? 1 : 0;
-        values.put(KEY_ENABLED, key_enabled);
-
-        values.put(KEY_CATEGORY, product.getCategory());
-
-        int i = db.update(TABLE_NAME, // table
-                values, // column/value
-                "name = ? AND category = ?", // selections
-                new String[] { oldname, String.valueOf(product.getCategory()) });
-
-        db.close();
-
-        return i;
-    }
-
-    public int updateProductsCategory(String oldcategoryname, String newcategoryname) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(KEY_CATEGORY, newcategoryname);
-
-        int i = db.update(TABLE_NAME, // table
-                values, // column/value
-                "category = ?", // selections
-                new String[] { oldcategoryname });
-
-        db.close();
-
-        return i;
-    }
-
-    public void deleteProduct(ObjProduct product) {
-        // Get reference to writable DB
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_NAME, "name = ?", new String[] { String.valueOf(product.getName()) });
-        db.close();
-    }
-
-    public void deleteProductsCategory(String categoryname) {
-        // Get reference to writable DB
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_NAME, "category = ?", new String[] { categoryname });
-        db.close();
-    }*/
 
     private ObjBillProduct writeProductList(Cursor cursor, int iTableCounter){
         ObjBillProduct objBillProduct = new ObjBillProduct();
 
         //set product
         for(ObjProduct objproduct : GlobVar.g_lstProduct){
-        if(objproduct.getName().equals(cursor.getString(6))){
-        objBillProduct.setProduct(objproduct);
-        }
+            if(objproduct.getName().equals(cursor.getString(6))){
+                objBillProduct.setProduct(objproduct);
+            }
         }
 
         objBillProduct.setCategory(cursor.getString(5));
@@ -238,31 +186,19 @@ public class SQLiteDatabaseHandler_TableBills extends SQLiteOpenHelper {
 
         //set printer
         for(ObjPrinter objprinter : GlobVar.g_lstPrinter){
-        if(objprinter.getMacAddress().equals(cursor.getString(8))){
-        objBillProduct.setPrinter(objprinter);
-        }
+            if(objprinter.getMacAddress().equals(cursor.getString(8))){
+                objBillProduct.setPrinter(objprinter);
+            }
         }
 
         //set printed
-        boolean bPrinted = true;
-        if(cursor.getString(9).equals("0")){
-        bPrinted = false;
-        }
-        objBillProduct.setPrinted(bPrinted);
+        objBillProduct.setPrinted(Integer.parseInt(cursor.getString(9)));
 
         //set canceled
-        boolean bCanceled = true;
-        if(cursor.getString(10).equals("0")){
-        bCanceled = false;
-        }
-        objBillProduct.setCanceled(bCanceled);
+        objBillProduct.setCanceled(Integer.parseInt(cursor.getString(10)));
 
         //set paid
-        boolean bPaid = true;
-        if(cursor.getString(11).equals("0")){
-        bPaid = false;
-        }
-        objBillProduct.setPaid(bPaid);
+        objBillProduct.setPaid(Integer.parseInt(cursor.getString(11)));
 
         return objBillProduct;
     }
--- app/src/main/java/objects/ObjBillProduct.java
+++ app/src/main/java/objects/ObjBillProduct.java
@@ -5,9 +5,11 @@ public class ObjBillProduct {
     private int m_iQuantity;
     private String m_strCategory;
     private ObjPrinter m_objPrinter;
-    private boolean m_bPrinted;
-    private boolean m_bCanceled;
-    private boolean m_bPaid;
+    private int m_iPrinted;
+    private int m_iCanceled;
+    private int m_iPaid;
+    private boolean m_bSqlSaved;
+    private boolean m_bSqlChanged;
 
     //getter
     public ObjProduct getProduct(){
@@ -22,14 +24,20 @@ public class ObjBillProduct {
     public ObjPrinter getPrinter(){
         return this.m_objPrinter;
     }
-    public boolean getPrinted(){
-        return this.m_bPrinted;
+    public int getPrinted(){
+        return this.m_iPrinted;
     }
-    public boolean getCanceled(){
-        return this.m_bCanceled;
+    public int getCanceled(){
+        return this.m_iCanceled;
     }
-    public boolean getPaid(){
-        return this.m_bPaid;
+    public int getPaid(){
+        return this.m_iPaid;
+    }
+    public boolean getSqlSaved() {
+        return m_bSqlSaved;
+    }
+    public boolean getqlChanged() {
+        return m_bSqlChanged;
     }
 
     //setter
@@ -45,13 +53,19 @@ public class ObjBillProduct {
     public void setPrinter(ObjPrinter p_objPrinter){
         this.m_objPrinter = p_objPrinter;
     }
-    public void setPrinted(boolean p_bPrinted){
-        this.m_bPrinted = p_bPrinted;
+    public void setPrinted(int p_bPrinted){
+        this.m_iPrinted = p_bPrinted;
     }
-    public void setCanceled(boolean p_bCanceled){
-        this.m_bCanceled = p_bCanceled;
+    public void setCanceled(int p_bCanceled){
+        this.m_iCanceled = p_bCanceled;
     }
-    public void setPaid(boolean p_bPaid){
-        this.m_bPaid = p_bPaid;
+    public void setPaid(int p_bPaid){
+        this.m_iPaid = p_bPaid;
+    }
+    public void setSqlSaved(boolean m_bSqlSaved) {
+        this.m_bSqlSaved = m_bSqlSaved;
+    }
+    public void setSqlChanged(boolean m_bSqlChanged) {
+        this.m_bSqlChanged = m_bSqlChanged;
     }
 }
