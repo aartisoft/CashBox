ce7ec47ec721db6120f5030903b2dde7a053bd9d
DDZenbook <diedrich.dominik@yahoo.com>
2019-04-22 15:50:18 +0200

    cash impl

---
+++ app/src/main/java/adapter/RecyclerViewBillCashAdapter.java
@@ -0,0 +1,141 @@
+package adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dd.cashbox.Main;
+import com.example.dd.cashbox.R;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import objects.ObjBillProduct;
+import objects.ObjMainBillProduct;
+
+public class RecyclerViewBillCashAdapter extends RecyclerView.Adapter<RecyclerViewBillCashAdapter.MyViewHolder>{
+    private Context context;
+    private List<ObjMainBillProduct> m_billproductList = new ArrayList<>();
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
+        public TextView textview_itemname;
+        public TextView textview_prize;
+        public TextView textview_printerQ;
+        public ImageView imageview_printer;
+        public View mCardView;
+
+        public MyViewHolder(@NonNull View view) {
+            super(view);
+
+            textview_itemname = view.findViewById(R.id.am_bill_rv_name);
+            textview_prize = view.findViewById(R.id.am_bill_rv_prize);
+            textview_printerQ = view.findViewById(R.id.am_bill_rv_printer_tv);
+            imageview_printer = view.findViewById(R.id.am_bill_rv_printerimage);
+
+            mCardView = view.findViewById(R.id.editproduct_recyclerview_items);
+            mCardView.setOnLongClickListener(this);
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            int position = (int) v.getTag();
+            ObjMainBillProduct objMainBillProduct = m_billproductList.get(position);
+            //implement interface instead if adapter is used in more than one activity!
+            ((Main)context).showRetoureStornoDialog(objMainBillProduct.getProduct().getCategory(), objMainBillProduct.getProduct().getName());
+            return false;
+        }
+    }
+
+    public RecyclerViewBillCashAdapter(Context context, List<ObjBillProduct> billproductList) {
+        this.context = context;
+
+
+        m_billproductList = new ArrayList<>();
+
+        //set shown false
+        for(ObjBillProduct objBillProduct : billproductList) {
+            objBillProduct.setShown(false);
+        }
+
+        //set list
+        for(ObjBillProduct objBillProductAdapter : billproductList){
+            if(!objBillProductAdapter.getPaid() && !objBillProductAdapter.getCanceled()
+                    && !objBillProductAdapter.getReturned() && !objBillProductAdapter.isShown()){
+                //init variables
+                ObjBillProduct objBillProductSearch = objBillProductAdapter;
+                ObjMainBillProduct objMainBillProduct = new ObjMainBillProduct();
+                objMainBillProduct.setProduct(objBillProductSearch.getProduct());
+                int iQuantity = 0;
+                int iPrinted = 0;
+                double dPrize = 0.0;
+                boolean bFound = false;
+
+                for(ObjBillProduct objBillProduct : billproductList){
+                    if(objBillProduct.getProduct() == objBillProductSearch.getProduct()){
+                        if(!objBillProduct.getPaid() && !objBillProduct.getCanceled()
+                                && !objBillProduct.getReturned() && !objBillProduct.isShown()){
+                            iQuantity++;
+                            dPrize += objBillProduct.getVK();
+                            if(objBillProduct.getPrinted()){
+                                iPrinted++;
+                            }
+                            objBillProduct.setShown(true);
+                            bFound = true;
+                        }
+                    }
+                }
+                if(bFound){
+                    objMainBillProduct.setQuantity(iQuantity);
+                    objMainBillProduct.setPrinted(iPrinted);
+                    objMainBillProduct.setVK(dPrize);
+
+                    //add to adapter list
+                    this.m_billproductList.add(objMainBillProduct);
+                }
+            }
+        }
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.am_bill_recyclerview_recyclerview_items, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.mCardView.setTag(position);
+
+        DecimalFormat df = new DecimalFormat("0.00");
+        final ObjMainBillProduct item = m_billproductList.get(position);
+
+        //set name
+        String strName = item.getQuantity() + "x " + item.getProduct().getName();
+        holder.textview_itemname.setText(strName);
+
+        //set prize
+        String strVK = df.format(item.getVK());
+        strVK = strVK + "€";
+        holder.textview_prize.setText(strVK);
+
+        //set image printer
+        String strPrinted = item.getPrinted() + "x";
+        holder.textview_printerQ.setText(strPrinted);
+        holder.imageview_printer.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public int getItemCount() {
+        return m_billproductList.size();
+    }
+}
--- app/src/main/java/com/example/dd/cashbox/MainCash.java
+++ app/src/main/java/com/example/dd/cashbox/MainCash.java
@@ -16,17 +16,34 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainCash extends AppCompatActivity{
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import SQLite.SQLiteDatabaseHandler_TableBills;
+import adapter.RecyclerViewBillCashAdapter;
+import adapter.RecyclerViewMainBillAdapter;
+import global.GlobVar;
+import objects.ObjBill;
+import objects.ObjBillProduct;
+
+public class MainCash extends AppCompatActivity implements View.OnClickListener {
 
     private Context m_Context;
     private View m_decorView;
+    private TextView m_TextViewTable;
+    private TextView m_TextViewBill;
+    private TextView m_TextViewOpenSum;
     private TextView m_TvToPay;
     private TextView m_TvBack;
     private EditText m_EdtWantsToPay;
     private EditText m_EdtPays;
     private Button m_btnPay;
     private Button m_btnCancel;
+    private RecyclerViewBillCashAdapter m_rv_adapter;
+    private RecyclerView m_recyclerview;
     private int m_iSessionTable = -1;
     private int m_iSessionBill = -1;
     private int m_uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -51,12 +68,16 @@ public class MainCash extends AppCompatActivity{
         //init variables
         m_Context = this;
         m_decorView = getWindow().getDecorView();
+        m_TextViewTable = findViewById(R.id.activity_main_bill_cash_tvtable);
+        m_TextViewBill = findViewById(R.id.activity_main_bill_cash_tvbill);
+        m_TextViewOpenSum = findViewById(R.id.activity_main_bill_cash_tvbillsum);
         m_TvToPay = findViewById(R.id.activity_main_cash_pay_tvtopaysum);
         m_TvBack = findViewById(R.id.activity_main_cash_pay_tvrestsum);
         m_EdtWantsToPay = findViewById(R.id.activity_main_cash_pay_edtcustomerwantspaysum);
         m_EdtPays = findViewById(R.id.activity_main_cash_pay_edtcustomerpayssum);
         m_btnPay = findViewById(R.id.activity_main_cash_pay_btnpay);
         m_btnCancel = findViewById(R.id.activity_main_cash_pay_btncancel);
+        m_recyclerview = findViewById(R.id.activity_main_bill_cash_rv);
 
         //set UI
         m_decorView.setSystemUiVisibility(m_uiOptions);
@@ -65,6 +86,10 @@ public class MainCash extends AppCompatActivity{
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //set headers
+        setHeaderTable();
+        setHeaderBill();
 
         //set Listener
         m_decorView.getViewTreeObserver().addOnGlobalLayoutListener(softkeyboardOnGlobalLayoutListener);
@@ -119,22 +144,6 @@ public class MainCash extends AppCompatActivity{
         }
     }
 
-    public View.OnTouchListener WantsToPayOnTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            m_EdtWantsToPay.setFocusableInTouchMode(true);
-            return false;
-        }
-    };
-
-    public View.OnTouchListener PaysOnTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            m_EdtPays.setFocusableInTouchMode(true);
-            return false;
-        }
-    };
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -149,5 +158,126 @@ public class MainCash extends AppCompatActivity{
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.activity_main_cash_pay_btnpay:
+
+                break;
+
+            case R.id.activity_main_cash_pay_btncancel:
+                Intent intent = new Intent(MainCash.this, Main.class);
+                intent.putExtra("BILL", m_iSessionBill);
+                intent.putExtra("TABLE", m_iSessionTable);
+                startActivity(intent);
+                finish();
+                break;
+
+            default:
+
+        }
+    }
+
+    //////////////////////////////////////////// METHODS /////////////////////////////////////////////////////////////////////////////
+    private void setHeaderTable(){
+        String strTableHeader = "";
+        if(m_iSessionTable != -1){
+            strTableHeader = getResources().getString(R.string.src_Tisch) + " " + String.valueOf(m_iSessionTable+1);
+        }
+        else{
+            strTableHeader = getResources().getString(R.string.src_Tisch_emtpy);
+        }
+        m_TextViewTable.setText(strTableHeader);
+    }
+
+    private void setHeaderBill(){
+        String strBillHeader = "";
+        if(m_iSessionBill != -1){
+
+            strBillHeader = getResources().getString(R.string.src_Beleg) + " " + String.valueOf(m_iSessionBill);
+
+            //only set recyclerview when bill product list not empty
+            if(GlobVar.g_lstTableBills.get(m_iSessionTable).get(getBillListPointer()).m_lstProducts == null){
+                GlobVar.g_lstTableBills.get(m_iSessionTable).get(getBillListPointer()).m_lstProducts = new ArrayList<ObjBillProduct>();
+            }
+            //set recyclerview
+            setupRecyclerView();
+        }
+        else{
+            strBillHeader = getResources().getString(R.string.src_Beleg_empty);
+        }
+        m_TextViewBill.setText(strBillHeader);
+    }
+
+    private void setOpenSum(){
+        String strOpenSum;
+        if(m_iSessionTable != -1 && m_iSessionBill != -1){
+            double prize = 0.00;
+            for(ObjBillProduct objBillProduct : GlobVar.g_lstTableBills.get(m_iSessionTable).get(getBillListPointer()).m_lstProducts) {
+                if(!objBillProduct.getPaid() && !objBillProduct.getCanceled() && !objBillProduct.getReturned()){
+                    prize += objBillProduct.getVK();
+                }
+            }
+
+            DecimalFormat df = new DecimalFormat("0.00");
+            strOpenSum = df.format(prize);
+            strOpenSum = strOpenSum + "€";
+        }
+        else{
+            strOpenSum = "0,00€";
+        }
+        m_TextViewOpenSum.setText(strOpenSum);
+    }
+
+    private void setupRecyclerView(){
+
+        //write tablebills to database
+        SQLiteDatabaseHandler_TableBills db_tablebills = new SQLiteDatabaseHandler_TableBills(m_Context);
+        db_tablebills.addTableBill(m_iSessionTable, m_iSessionBill);
+
+        //get list bill pointer
+        final int iBill = getBillListPointer();
+
+        m_rv_adapter = new RecyclerViewBillCashAdapter(this, GlobVar.g_lstTableBills.get(m_iSessionTable).get(iBill).m_lstProducts);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        m_recyclerview.setLayoutManager(mLayoutManager);
+
+        m_recyclerview.setAdapter(m_rv_adapter);
+        m_rv_adapter.notifyDataSetChanged();
+
+        //set infotext mainbill
+        if(GlobVar.g_lstTableBills.get(m_iSessionTable).get(iBill).m_lstProducts.size() > 0){
+            boolean bFound = false;
+            for(ObjBillProduct objBillProduct : GlobVar.g_lstTableBills.get(m_iSessionTable).get(iBill).m_lstProducts){
+                if(!objBillProduct.getPaid() && !objBillProduct.getCanceled() && !objBillProduct.getReturned()){
+                    bFound = true;
+                    break;
+                }
+            }
+            if(bFound){
+                findViewById(R.id.activity_main_bill_cash_rv_noitem).setVisibility(View.VISIBLE);
+            }
+            else{
+                findViewById(R.id.activity_main_bill_cash_rv_noitem).setVisibility(View.INVISIBLE);
+            }
+        }
+        else{
+            findViewById(R.id.activity_main_bill_cash_rv_noitem).setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private int getBillListPointer(){
+        //get bill
+        int iBill = 0;
+        for(ObjBill objBill : GlobVar.g_lstTableBills.get(m_iSessionTable)){
+            if(objBill.getBillNr() == m_iSessionBill){
+                return iBill;
+            }
+            iBill++;
+        }
+        return 0;
     }
 }
