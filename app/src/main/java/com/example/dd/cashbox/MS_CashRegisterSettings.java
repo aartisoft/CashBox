5f39cda2024da366227ee33d1c166165ad2fc142
Dominik <diedrich.dominik@yahoo.com>
2019-05-22 19:51:02 +0200

    settings impl

--- app/src/main/java/com/example/dd/cashbox/MS_CashRegisterSettings.java
+++ app/src/main/java/com/example/dd/cashbox/MS_CashRegisterSettings.java
@@ -20,4 +20,101 @@ import android.view.MenuItem;
 import preferences.AppCompatPreferenceActivity;
 
 public class MS_CashRegisterSettings extends AppCompatPreferenceActivity {
+    private static final String TAG = MS_CashRegisterSettings.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // load settings fragment
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreferenceFragment()).commit();
+    }
+
+    public static class MainPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_cashregistersettings);
+
+            // gallery EditText change listener
+            //bindPreferenceSummaryToValue(findPreference(getString(R.string.key_gallery_name)));
+
+            // notification preference change listener
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.src_Kasseneinstellungen)));
+
+            // feedback preference click listener
+            /*Preference myPref = findPreference(getString(R.string.src_BonsDrucken));
+            myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    sendFeedback(getActivity());
+                    return true;
+                }
+            });*/
+        }
+    }
+
+    /////////////////////////// LISTENER /////////////////////////////////////////////////////////////////////////////////////
+
+    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            String stringValue = newValue.toString();
+
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list.
+                ListPreference listPreference = (ListPreference) preference;
+                int index = listPreference.findIndexOfValue(stringValue);
+
+                // Set the summary to reflect the new value.
+                preference.setSummary(
+                        index >= 0
+                                ? listPreference.getEntries()[index]
+                                : null);
+
+            } else if (preference instanceof RingtonePreference) {
+                // For ringtone preferences, look up the correct display value
+                // using RingtoneManager.
+                if (TextUtils.isEmpty(stringValue)) {
+                    // Empty values correspond to 'silent' (no ringtone).
+                    preference.setSummary(R.string.src_BonsDrucken);
+
+                } else {
+                    Ringtone ringtone = RingtoneManager.getRingtone(
+                            preference.getContext(), Uri.parse(stringValue));
+
+                    if (ringtone == null) {
+                        // Clear the summary if there was a lookup error.
+                        preference.setSummary(R.string.src_BonsDrucken);
+                    } else {
+                        // Set the summary to reflect the new ringtone display
+                        // name.
+                        String name = ringtone.getTitle(preference.getContext());
+                        preference.setSummary(name);
+                    }
+                }
+
+            } else if (preference instanceof EditTextPreference) {
+                if (preference.getKey().equals("key_gallery_name")) {
+                    // update the changed gallery name to summary filed
+                    preference.setSummary(stringValue);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
+        }
+    };
+
+    /////////////////////////// METHODS /////////////////////////////////////////////////////////////////////////////////////
+
+    private static void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
 }
