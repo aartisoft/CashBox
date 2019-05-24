c9472598c398d25e3760536b947f610c30dce4b6
raxtorDD <46646548+raxtorDD@users.noreply.github.com>
2019-05-24 11:20:18 +0200

    Update PrintJobQueue.java
    improve bugfix

--- app/src/main/java/printer/PrintJobQueue.java
+++ app/src/main/java/printer/PrintJobQueue.java
@@ -123,7 +123,7 @@ public class PrintJobQueue{
                                     //try 10 times to print data
                                     do{
                                         //Log.e("Printer ",m_lstPrinterJob.get(iPrintJobCounter).getBillText()[8]);
-                                        m_bPrintStatus = m_EpsonPrintBill.runPrintBillSequence(m_lstPrinterJob.get(iPrintJobCounter).getBillText());
+                                        m_bPrintStatus = m_EpsonPrintBill.runPrintBillSequence(m_lstPrinterJob.get(iPrintJobCounter).g_lstBillText);
 
                                         //if printing process was successfull
                                         if(m_bPrintStatus){
