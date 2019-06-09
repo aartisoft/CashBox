31d755b58cf62245d4a1161572adea0107f42e0f
DDZenbook <diedrich.dominik@yahoo.com>
2019-06-09 22:49:47 +0200

    login impl

---
+++ app/src/main/java/objects/ObjLoginUser.java
@@ -0,0 +1,34 @@
+package objects;
+
+import java.util.Date;
+
+
+public class ObjLoginUser {
+    String username;
+    String fullName;
+    Date sessionExpiryDate;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setSessionExpiryDate(Date sessionExpiryDate) {
+        this.sessionExpiryDate = sessionExpiryDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public Date getSessionExpiryDate() {
+        return sessionExpiryDate;
+    }
+}
